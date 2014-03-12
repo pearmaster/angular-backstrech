70cd547f514828234e59375fb699d896bf683ec9
Jacob Brunson <github@jacobbrunson.com>
2014-03-12 11:27:22 -0600

    fixed missing comma

--- angular-backstretch.js
+++ angular-backstretch.js
@@ -1,9 +1,9 @@
 angular.module('backstretch')
-	.directive('backstretch', function () {
-		return {
-			restrict: 'A'
-			link: function (scope, element, attr) {
+.directive('backstretch', function () {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attr) {
 				$(element).backstretch(attr.backgroundUrl);
-			}
 		}
-	});
+	}
+});
